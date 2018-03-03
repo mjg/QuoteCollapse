60275102aeb7fb33900753df774c745cf4170c78
Stanimir Stamenkov <stanio@yahoo.com>
2018-03-03 16:46:31 -0500

    Expand active spot through whole of the vertical quote border

--- src/chrome/content/quotecollapse/quotecollapse.js
+++ src/chrome/content/quotecollapse/quotecollapse.js
@@ -166,7 +166,7 @@ blockquote[type="cite"]:not([qctoggled="true"]) blockquote[type="cite"] {\n\
 
 
 // react only to active spot (leave rest for copy etc.)
-    if( (event.pageX > target.offsetLeft+12) || (event.pageY > target.offsetTop+12) ) return true;
+    if(event.pageX > target.offsetLeft+12) return true;
     
     if(event.shiftKey)
       if(event.ctrlKey || event.metaKey)
